603a75bb6103d84af6eabf2c73dbc3e7714e8117
JeffAllison <itsjallison@gmail.com>
2022-06-28 11:52:55 -0700

    attempt at binary tree add

--- binaryTree.js
+++ binaryTree.js
@@ -1,13 +1,38 @@
 class BinaryTreeNode {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor(value) {
+    this.root = new BinaryTreeNode(value);
+  }
+
+  add(newNode, current) {
+    if (newNode >= current.value) {
+      if (!current.right) {
+        current.right = new BinaryTreeNode(newNode);
+      } else {
+        this.add(newNode, current.right);
+      }
+    } else {
+      if (!current.left) {
+        current.left = new BinaryTreeNode(newNode);
+      } else {
+        this.add(newNode, current.left);
+      }
     }
   }
-  
-  class BinaryTree {
-    constructor(value) {
-      this.root = new BinaryTreeNode(value);
-    }
-  }
+}
+
+const list = new BinaryTree('C');
+console.log(list);
+list.add('B', this.root);
+console.log(list);
+list.add('A', this.root);
+console.log(list);
+list.add('D', this.root);
+console.log(list);
